d50b38679ae1c9f0f04a08d6f6c9729f0c948b2a
jaalma <james@jmaclachlan.me>
2023-05-06 15:38:08 -0400

    add HTML head to main app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,26 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import type { AppType } from "next/app";
+import Head from "next/head";
 import { api } from "~/utils/api";
 import { Navbar } from "~/components/Navbar";
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <ClerkProvider {...pageProps}>
-      <div className="max-w-screen min-w-screen flex max-h-screen min-h-screen flex-col">
-        <Navbar />
-        <main className="flex flex-grow bg-gray-800 py-4 text-neutral-100">
-          <Component {...pageProps} />
-        </main>
-      </div>
-    </ClerkProvider>
+    <>
+      <Head>
+        <title>Threatdash</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <ClerkProvider {...pageProps}>
+        <div className="max-w-screen min-w-screen flex max-h-screen min-h-screen flex-col">
+          <Navbar />
+          <main className="flex flex-grow bg-gray-800 py-4 text-neutral-100">
+            <Component {...pageProps} />
+          </main>
+        </div>
+      </ClerkProvider>
+    </>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,17 @@
 import type { NextPage } from "next";
 import Link from "next/link";
-import Head from "next/head";
 
 const Home: NextPage = () => {
   return (
-    <>
-      <Head>
-        <title>Threatdash</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className="m-auto flex max-h-screen min-h-full min-w-full flex-col items-center justify-center">
-        <h1 className="text-3xl text-white">Welcome to Threatdash</h1>
-        <Link
-          href={"/feed"}
-          className="m-4 text-2xl text-neutral-300 hover:animate-none hover:text-neutral-100 motion-safe:animate-pulse"
-        >
-          Get Started →
-        </Link>
-      </div>
-    </>
+    <div className="m-auto flex max-h-screen min-h-full min-w-full flex-col items-center justify-center">
+      <h1 className="text-3xl text-white">Welcome to Threatdash</h1>
+      <Link
+        href={"/feed"}
+        className="m-4 text-2xl text-neutral-300 hover:animate-none hover:text-neutral-100 motion-safe:animate-pulse"
+      >
+        Get Started →
+      </Link>
+    </div>
   );
 };
 
