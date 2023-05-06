a8d001f69fc852fca045259b772c82bb73f43036
jaalma <james@jmaclachlan.me>
2023-05-06 14:59:52 -0400

    remove unused import

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { SignInButton, UserButton, useUser } from "@clerk/nextjs";
+import { UserButton, useUser } from "@clerk/nextjs";
 import Link from "next/link";
 
 export const Navbar: React.FC = () => {
