8c305c4fca2d93f90efe6e4e1950951d09f82460
jaalma <james@jmaclachlan.me>
2023-05-22 15:29:41 -0400

    wrap dashboard loading with layout

--- src/pages/dashboard/loading.tsx
+++ src/pages/dashboard/loading.tsx
@@ -1,14 +1,18 @@
+import { EventsLayout } from "./layout";
+
 export const DashboardLoading: React.FC = () => {
   return (
-    <div className="mx-16 mb-8 flex h-full flex-col">
-      <div className="flex h-1/2 w-full flex-row py-4">
-        <div className="mr-4 h-full w-1/3 animate-pulse rounded-lg bg-gray-500"></div>
-        <div className="mx-4 h-full w-1/3 animate-pulse rounded-lg bg-gray-500"></div>
-        <div className="ml-4 h-full w-1/3 animate-pulse rounded-lg bg-gray-500"></div>
+    <EventsLayout>
+      <div className="mx-16 mb-8 flex h-full flex-col">
+        <div className="flex h-1/2 w-full flex-row py-4">
+          <div className="mr-4 h-full w-1/3 animate-pulse rounded-lg bg-gray-500"></div>
+          <div className="mx-4 h-full w-1/3 animate-pulse rounded-lg bg-gray-500"></div>
+          <div className="ml-4 h-full w-1/3 animate-pulse rounded-lg bg-gray-500"></div>
+        </div>
+        <div className="mb-8 h-1/2 py-4">
+          <div className="h-full animate-pulse rounded-lg bg-gray-500"></div>
+        </div>
       </div>
-      <div className="mb-8 h-1/2 py-4">
-        <div className="h-full animate-pulse rounded-lg bg-gray-500"></div>
-      </div>
-    </div>
+    </EventsLayout>
   );
 };
