6b088a600675d933a87104a5b5021d593886a474
jaalma <james@jmaclachlan.me>
2023-05-24 10:43:50 -0400

    add total URL count to event dashboard

--- src/components/DashboardStatBox.tsx
+++ src/components/DashboardStatBox.tsx
@@ -7,7 +7,7 @@ export const DashboardStatBox: React.FC<dashboardStatBoxProps> = (
   props: dashboardStatBoxProps
 ) => {
   return (
-    <div className="mx-4 flex h-full w-1/3 flex-col rounded-lg border-2 border-slate-200 p-6 text-center font-semibold shadow-lg">
+    <div className="mx-4 flex h-full flex-1 flex-col rounded-lg border-2 border-slate-200 p-6 text-center font-semibold shadow-lg">
       {props.displayName}
       <span className="m-auto text-6xl">{props.displayValue}</span>
     </div>
